bb87f511b6913374d88162c51775e7256a41d097
ht19a006 <91107419+ht19a006@users.noreply.github.com>
2021-12-09 21:32:53 +0900

    Create ex-1-main.cpp

---
+++ ex-1-main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#define CPP2_PRIME_UPPER_LIMIT 1000000 //探索する値の上限値。
+/* --------------------------------------------------------------- */
+/*
+* nth_prime
+*
+* 与えられた正整数 a と d と n に対して、この等差数列に含まれる n 番目の
+* 素数を返す。
+*
+* TODO: この nth_prime 関数を実装せよ。必要であれば他に関数や
+* ファイルを作成せよ。
+*/
+/* -------------------------------------------------------------- */
+int nth_prime(unsigned int a, unsigned int d, unsigned int n)
+{
+	std::cin >> a;//初項の入力
+	std::cin >> d;//公差の入力
+	std::cin >> n;//n番目の素数（何番目の素数を求めるかの入力）
+	for (int i = 0; i < n; i++)
+	{
+		int a1 = 0;
+		a1 = a + n * d;
+
+		if (a1 < 2)
+		
+			return 0;
+		
+		if (a1 == 2)
+			return 1;
+
+		int k;
+		for (k = 2; k <= a1; k++)
+		{
+			if (a1 % k == 0)
+				return 0;
+		}
+		return 1;
+
+	}
+
+	return 0;
+}
+
+
+int main() {
+	std::cout << nth_prime(367, 186, 151) << std::endl;
+	// 以下、同様に、入出力例通りになるか確認せよ。
+	return 0;
+}
